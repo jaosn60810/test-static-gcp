839326f17fa951699c8069c340462da2beebcae7
jasonchien <jasonchien@cela-tech.com>
2023-12-07 15:10:04 +0800

    Add basePath and output configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/out',
+  output: 'export',
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
